ec1886e9dd0493a14da0b42c34a33ad26193d1d2
Sergio Henrique <sergiohenriquemarques@outlook.com>
2016-08-25 21:56:49 -0300

    Adiciona exemplo de impressao

---
+++ src/br/ufjf/dcc171/Principal.java
@@ -0,0 +1,16 @@
+package br.ufjf.dcc171;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        Livro l1 = new Livro("Java com programar", 1977, 50, 123.25F);
+        Livro l2 = new Livro("Java com programar 2", 1980, 60, 156.25F);
+        System.out.println("Livros disponiveis: ");
+        System.out.println(l1.getTitulo());
+        System.out.print(l1.getAno());
+        System.out.print(l1.getPaginas() + " pgs ");
+        System.out.println("R$ " + l1.getPreco());
+        System.out.println(l1.toString());
+    }
+    
+}
